0269001c5ff221285c1edecc566dfbb86f551e38
robertpanvip <robertpanvip@163.com>
2026-03-15 23:36:06 +0800

    feat:添加version manage

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.pan"
-version = "1.2.2"
+version = "1.3.0"
 
 repositories {
     mavenCentral()
@@ -35,7 +35,7 @@ intellijPlatform {
         }
 
         changeNotes = """
-      Initial version
+      add Version manage
     """.trimIndent()
     }
 }
